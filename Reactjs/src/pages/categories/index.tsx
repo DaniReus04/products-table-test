60e2dfc3d5bb3efd75fb8663b946d75c4ee425a5
Daniel C. <daniel_carvalho_amaroo@outlook.com>
2025-02-26 07:24:16 -0300

    feat: categories page

--- Reactjs/src/pages/categories/index.tsx
+++ Reactjs/src/pages/categories/index.tsx
@@ -1,9 +1,53 @@
-import React, { FC } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from "@mui/material";
+import { Category } from "../../interfaces/category";
 
-interface CategoriesProps {}
+const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
 
-const Categories: FC<CategoriesProps> = () => {
-  return <div>Categories</div>;
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/categories")
+      .then((res) => setCategories(res.data));
+  }, []);
+
+  return (
+    <Container>
+      <h1>Categories</h1>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {categories.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell>{category.name}</TableCell>
+              <TableCell>
+                <Button variant="contained" color="primary">
+                  Edit
+                </Button>
+                <Button variant="contained" color="secondary">
+                  Delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
 };
 
 export default Categories;
