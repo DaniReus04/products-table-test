ca38468a5cb40f40ba7fd90c46f383fd3cffc36c
Daniel C. <daniel_carvalho_amaroo@outlook.com>
2025-02-26 07:23:40 -0300

    feat: products page

--- Reactjs/src/pages/prodcuts/index.tsx
+++ Reactjs/src/pages/prodcuts/index.tsx
@@ -1,9 +1,55 @@
-import React, { FC } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from "@mui/material";
+import { Product } from "../../interfaces/produtcts";
 
-interface ProductsProps {}
+const Products = () => {
+  const [products, setProducts] = useState<Product[]>([]);
 
-const Products: FC<ProductsProps> = () => {
-  return <div>Products</div>;
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((res) => setProducts(res.data));
+  }, []);
+
+  return (
+    <Container>
+      <h1>Products</h1>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>${product.price}</TableCell>
+              <TableCell>
+                <Button variant="contained" color="primary">
+                  Edit
+                </Button>
+                <Button variant="contained" color="secondary">
+                  Delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
 };
 
 export default Products;
