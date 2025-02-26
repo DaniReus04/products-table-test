d1f7ac3a39101e4027b64719defda5dfdf401bb9
Daniel C. <daniel_carvalho_amaroo@outlook.com>
2025-02-26 07:24:51 -0300

    feat: header and footer information

--- Reactjs/src/components/footer/index.tsx
+++ Reactjs/src/components/footer/index.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from "react";
+import { Box, Typography } from "@mui/material";
 
-interface FooterProps {}
-
-const Footer: FC<FooterProps> = () => {
-  return <div>Footer</div>;
+const Footer = () => {
+  return (
+    <Box sx={{ textAlign: "center", p: 2, bgcolor: "#f4f4f4", mt: 4 }}>
+      <Typography variant="body2">
+        © 2025 Daniel Carvalho. All rights reserved.
+      </Typography>
+    </Box>
+  );
 };
 
 export default Footer;
--- Reactjs/src/components/header/index.tsx
+++ Reactjs/src/components/header/index.tsx
@@ -1,9 +1,25 @@
-import React, { FC } from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
-interface HeaderProps {}
-
-const Header: FC<HeaderProps> = () => {
-  return <div>Header</div>;
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          E-commerce Dashboard
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Products
+        </Button>
+        <Button color="inherit" component={Link} to="/categories">
+          Categories
+        </Button>
+        <Button color="inherit" component={Link} to="/orders">
+          Orders
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
